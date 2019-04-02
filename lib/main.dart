d278d1ba094f5412ba35532984001705453bf02d
joypaul <jpaul3250@gmail.com>
2019-04-02 14:11:27 -0400

    some comments

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(), //theme is dark so the animation is visible
       home: MyHomePage(),
     );
   }
 }
 
+//using flare actor to display flare animation
 Widget buildAnimation(){
   return FlareActor("assets/Trim.flr",
     fit: BoxFit.cover,
     alignment: Alignment.center,
-    animation: "Untitled",
+    animation: "Untitled", //the name of the animation
   );
 }
 
